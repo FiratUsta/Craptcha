bb898dfbbee6ecee003f2b8dbb1b37bda30119d2
FiratUsta <ahmetfiratusta@gmail.com>
2024-10-16 01:37:22 +0300

    Update test bot.

--- scripts/craptcha.js
+++ scripts/craptcha.js
@@ -35,7 +35,7 @@ class Challenge{
             case Elements.inputText:
                 return Elements.inputText.value.toLowerCase();
             case Elements.inputRange:
-                return Elements.inputRange.value;
+                return parseInt(Elements.inputRange.value);
             default:
                 return null;
         }
@@ -82,8 +82,8 @@ class Craptcha{
 
         // Consts
         this.botThreshold = 20;
-        this.keysLeft = "qwertasdfgyxcvb";
-        this.keysRight = "zuiopühjklönm,.";
+        this.keysLeft = "qwertasdfgyxcvb12345";
+        this.keysRight = "zuiopühjklönm,.67890";
         this.challenges = [coinFlips, numberPicker, keyboardMash, mashRepeat];
 
         this.init();
@@ -122,7 +122,7 @@ class Craptcha{
         Elements.buttonChallenge.onclick = () => {
             if(this.currentChallenge === this.challenges.length - 1){
                 this.answers.push(challenge.getAnswer());
-                this.checkResults();
+                this.displayResults();
             }else{
                 this.answers.push(challenge.getAnswer());
                 this.currentChallenge++;
@@ -131,9 +131,94 @@ class Craptcha{
         }
     }
 
-    checkResults(){
-        console.log("Done");
+    // Result calculators
+    coinFlipChecker(answer){
+        let score = 0;
+        for(let i = 3;i<8;i++){
+            let substrHeads = "";
+            let substrTails = "";
+            for(let j=0;j<i;j++){
+                substrHeads += "h";
+                substrTails += "t";
+            }
+            if(answer.includes(substrHeads) || answer.includes(substrTails)){
+                score += 9;
+            }
+        }
+        return score;
+    }
+
+    randomNumberChecker(answer){
+        switch(answer){
+            case 0:
+                return 9.3;
+            case 1:
+                return 5.6;
+            case 2:
+                return 1.5;
+            case 3:
+                return -0.7;
+            case 4:
+                return -0.7;
+            case 5:
+                return -3.3;
+            case 6:
+                return -0.8;
+            case 7:
+                return -19;
+            case 8:
+                return -1.8;
+            case 9:
+                return 3.7;
+            case 10:
+                return 7.1;
+        }
+    }
+
+    keySmashChecker(answer){
+        let lefts = 0;
+        let rights = 0;
+        for(let i = 0; i < answer.length; i++){
+            if(this.keysLeft.includes(answer.charAt(i))){
+                lefts++;
+            }else if(this.keysRight.includes(answer.charAt(i))){
+                rights++
+            }
+        }
+        const percentLeft = (lefts/answer.length) * 100;
+        const score = Math.abs(percentLeft - 50);
+        return score;
+    }
+
+    checkResults(answers){
+        const coinScore = this.coinFlipChecker(answers[0]);
+        const numberScore = this.randomNumberChecker(answers[1]);
+        const smashScoreFirst = this.keySmashChecker(answers[2]);
+        const smashScoreSecond = this.keySmashChecker(answers[3]);
+        const totalScore = coinScore + numberScore + smashScoreFirst + smashScoreSecond;
+
+        return{
+            "Coin Flip Challenge": coinScore,
+            "Random Number Challenge": numberScore,
+            "First Keysmash Challenge": smashScoreFirst,
+            "Second Keysmash Challenge": smashScoreSecond,
+            "Total Score": totalScore,
+            "Is A Bot?": (totalScore > this.botThreshold)
+        }
+    }
+
+    displayResults(){
+        Elements.craptchaChallenge.classList.remove("visible");
+        const results = this.checkResults(this.answers);
         console.log(this.answers);
+        console.log(results)
+        if(results["Is A Bot?"]){
+            Elements.fieldMessage.innerText = "CRAPTCHA failed, please refresh the page and try again!";
+        }else{
+            Elements.fieldMessage.innerText = "CRAPTCHA passed, click to log in button to log in!";
+            Elements.fieldMessage.style.color = "green";
+            Elements.buttonLogin.disabled = false;
+        }
     }
 }
 
