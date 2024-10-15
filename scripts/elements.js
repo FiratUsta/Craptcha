83330de88e79eaf570b56180f877811565c6a0bf
FiratUsta <ahmetfiratusta@gmail.com>
2024-10-15 23:30:04 +0300

    Create elements.js and change some IDs

---
+++ scripts/elements.js
@@ -0,0 +1,35 @@
+// Form Elems
+const inputUsername     = document.getElementById("inputUsername");
+const inputPassword     = document.getElementById("inputPassword");
+const buttonLogin       = document.getElementById("buttonLogin");
+// Challenge Elems
+const challengePrompt   = document.getElementById("challengePrompt");
+const challengeNumber   = document.getElementById("challengeNumber");
+const challengeLabel    = document.getElementById("challengeLabel");
+const progressBar       = document.getElementById("progressBar");
+const inputText         = document.getElementById("inputText");
+const inputRange        = document.getElementById("inputRange");
+const buttonChallenge   = document.getElementById("buttonChallenge");
+// Craptcha Misc
+const craptchaMain      = document.getElementById("craptchaMain");
+const checkboxContainer = document.getElementById("checkboxContainer");
+const inputCheckbox     = document.getElementById("inputChechbox");
+
+export{
+    // Form Elems
+    inputUsername,
+    inputPassword,
+    buttonLogin,
+    // Challenge Elems,
+    challengePrompt,
+    challengeNumber,
+    challengeLabel,
+    progressBar,
+    inputText,
+    inputRange,
+    buttonChallenge,
+    // Craptcha Misc
+    craptchaMain,
+    checkboxContainer,
+    inputCheckbox     
+}
