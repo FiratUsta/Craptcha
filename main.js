ef266fd8a12177b131123575ec68414261e2f4da
FiratUsta <ahmetfiratusta@gmail.com>
2024-10-15 23:40:38 +0300

    Modify main.js to fit the new structure.

--- main.js
+++ main.js
@@ -3,13 +3,7 @@ import { Craptcha } from "./scripts/craptcha.js";
 import { TestBot } from "./scripts/testBot.js";
 
 const craptcha = new Craptcha(document.getElementById("craptcha"));
-
-const validator = new FormValidator(document.getElementById("username"), 
-                                document.getElementById("password"),
-                                document.getElementById("message"),
-                                document.getElementById("loginButton"),
-                                craptcha);
-
+const validator = new FormValidator(craptcha);
 const testBot = new TestBot(craptcha);
 
 //testBot.runTests(1000000);
