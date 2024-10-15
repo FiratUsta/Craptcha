f1c7b7fecd3b98db10cd32abcd4b21ef653f8f81
FiratUsta <ahmetfiratusta@gmail.com>
2024-10-16 00:52:38 +0300

    Fix main.js to reflect new structure.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import { FormValidator } from "./scripts/validator.js";
 import { Craptcha } from "./scripts/craptcha.js";
-import { TestBot } from "./scripts/testBot.js";
+//import { TestBot } from "./scripts/testBot.js";
 
-const craptcha = new Craptcha(document.getElementById("craptcha"));
-const validator = new FormValidator(craptcha);
-const testBot = new TestBot(craptcha);
+const validator = new FormValidator();
+const craptcha = new Craptcha();
 
+// Testing
+//const testBot = new TestBot(craptcha);
 //testBot.runTests(1000000);
