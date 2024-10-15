aeb32659b8d9a9319aee82a37ec8b6fe50188daf
FiratUsta <ahmetfiratusta@gmail.com>
2024-10-15 23:43:21 +0300

    Add skeleton vars and consts

--- scripts/craptcha.js
+++ scripts/craptcha.js
@@ -1,6 +1,36 @@
 import * as Elements from "./elements.js";
 
 class Craptcha{
+    constructor(){
+        // Vars
+        this.botThreshold;
+        this.answers;
+
+        // Consts
+        this.keysLeft;
+        this.keysRight;
+        this.challenges;
+    }
+
+    init(){
+
+    }
+
+    prepareChallenge(){
+
+    }
+
+    presentChallenges(){
+
+    }
+
+    checkResults(){
+
+    }
+
+    presentResults(){
+
+    }
 
 }
 
