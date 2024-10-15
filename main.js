922fcdf4f7b2c72ab63a0236de2c4199f1d0f56d
FiratUsta <ahmetfiratusta@gmail.com>
2024-10-16 01:36:55 +0300

    Update main.js to disable testing.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { FormValidator } from "./scripts/validator.js";
 import { Craptcha } from "./scripts/craptcha.js";
-//import { TestBot } from "./scripts/testBot.js";
+import { TestBot } from "./scripts/testBot.js";
 
 const validator = new FormValidator();
 const craptcha = new Craptcha();
